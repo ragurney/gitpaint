c1c4904b3e01d36918b64ba19602dc5460df0715
Ryan Gurney <ryan.a.gurney@gmail.com>
2016-01-02 14:20:16 -0800

    Made some progress on the bat file. Lots of things temporarily hard-coded

--- PaintDriver.py
+++ PaintDriver.py
@@ -1,23 +1,27 @@
 import configparser
 import logging
 import os
-from subprocess import check_output
+import subprocess
 
 Config = configparser.ConfigParser()
-#configSections = ["Git Exe Options", "Dummy Repo Options"]
+# configSections = ["Git Exe Options", "Dummy Repo Options"]
 
+dummyFileName = 'dummyFile.txt'
+batchFileName = 'dummyUpdateAndPush.bat'
+
+
+batchFileCreated = False
 gitLocation = ""
 dummyRepoPath = ""
 dummyFilePath = ""
 
 # Bash Commands
 #  TODO: Add more info (date, part of letter done), also, definitely change this string lol
-bashCD =  "cd {}\n"
-bashGitAddFile = "{} {}add -A dummyFile.txt\n"
-            #"{} commit -m \"File Updated\"\n" \
-            #"{} push origin master\n" \
-            #"start chrome.exe\n"
-
+batchFile = "cd {}\n" \
+           "{} add -A dummyFile.txt\n" \
+           "{} commit -m \"File Updated\"\n" \
+           "{} push origin master\n" \
+           "start chrome.exe\n"
 
 vars = [gitLocation, dummyRepoPath, dummyFilePath]
 
@@ -34,35 +38,42 @@ vars = [gitLocation, dummyRepoPath, dummyFilePath]
             dict1[option] = None
     return dict1 """
 
-#def setup():
-    # Could later extend to log part of letter drawn with that commit
+
+def create_bash_file(batchFileCreated, gitLocation, dummyRepoPath):
+    if not batchFileCreated:
+        # Create batch File
+        file = open(batchFileName, 'w')
+        file.write("ECHO OFF\n\n" + batchFile.format(dummyRepoPath,
+                                                     gitLocation, gitLocation, gitLocation))
+        file.close()
+        Config.set("File Status", "BAT Created", "True")
 
 
 def loggingsetup():
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
 
+
 def run_bash_commands(gitPath, dummyRepoPath, dummyFilePath):
     bashCommand = "start chrome.exe"
-    check_output(bashCommand, shell=True)
+
 
 def update_dummy_file(gitLocation, dummyRepoPath):
-    file = open(os.path.join(dummyRepoPath,'dummyFile.txt'), 'w')
+    file = open(os.path.join(dummyRepoPath, 'dummyFile.txt'), 'w')
     file.write(" ")
     file.close()
 
-    # execute bash commands
-    #print(bashFile.format(dummyRepoPath, gitLocation, gitLocation, gitLocation))
-   # check_output((bashFile.format(dummyRepoPath, gitLocation, gitLocation, gitLocation)), shell=True)
-    check_output(bashCD.format(dummyRepoPath), shell=True)
-    check_output(bashGitAddFile.format(gitLocation), shell=True)
-#-A, commit, push
 
 def main():
     Config.read(".\\config.ini")
-
-    #forsaking usability for hardcoding, but added efficiency //TODO: be smarterer
+    # forsaking usability for hardcoding, but added efficiency //TODO: be smarterer
     gitLocation = Config["Path Options"]["Git Exe Path"]
     dummyRepoPath = Config["Path Options"]["Dummy Repo Path"]
+    batchFileCreated = Config.getboolean("File Status", "BAT Created")
+
+    # Create batch file for file updates if needed
+    create_bash_file(batchFileCreated, gitLocation, dummyRepoPath)
+
     update_dummy_file(gitLocation, dummyRepoPath)
+
 
 main()
